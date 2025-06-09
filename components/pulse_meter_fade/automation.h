6623048feb2cf21235b97c083dac583300d6f5df
VeeoN <veeon@op.pl>
2025-06-09 18:41:05 +0200

    More fixes

--- components/pulse_meter_fade/automation.h
+++ components/pulse_meter_fade/automation.h
@@ -2,22 +2,22 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
-#include "esphome/components/pulse_meter/pulse_meter_sensor.h"
+#include "esphome/components/pulse_meter_fade/pulse_meter_sensor.h"
 
 namespace esphome {
 
-namespace pulse_meter {
+namespace pulse_meter_fade {
 
 template<typename... Ts> class SetTotalPulsesAction : public Action<Ts...> {
  public:
-  SetTotalPulsesAction(PulseMeterSensor *pulse_meter) : pulse_meter_(pulse_meter) {}
+  SetTotalPulsesAction(PulseMeterFadeSensor *pulse_meter) : pulse_meter_(pulse_meter) {}
 
   TEMPLATABLE_VALUE(uint32_t, total_pulses)
 
   void play(Ts... x) override { this->pulse_meter_->set_total_pulses(this->total_pulses_.value(x...)); }
 
  protected:
-  PulseMeterSensor *pulse_meter_;
+  PulseMeterFadeSensor *pulse_meter_;
 };
 
 }  // namespace pulse_meter
