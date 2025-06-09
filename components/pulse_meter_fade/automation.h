79d3e9ad843910a05e3a5b378321a2d7b77c4245
VeeoN <veeon@op.pl>
2025-06-09 18:45:14 +0200

    fixes #4

--- components/pulse_meter_fade/automation.h
+++ components/pulse_meter_fade/automation.h
@@ -10,15 +10,15 @@ namespace pulse_meter_fade {
 
 template<typename... Ts> class SetTotalPulsesAction : public Action<Ts...> {
  public:
-  SetTotalPulsesAction(PulseMeterFadeSensor *pulse_meter) : pulse_meter_(pulse_meter) {}
+  SetTotalPulsesAction(PulseMeterFadeSensor *pulse_meter_fade) : pulse_meter_(pulse_meter) {}
 
   TEMPLATABLE_VALUE(uint32_t, total_pulses)
 
-  void play(Ts... x) override { this->pulse_meter_->set_total_pulses(this->total_pulses_.value(x...)); }
+  void play(Ts... x) override { this->pulse_meter_fade_->set_total_pulses(this->total_pulses_.value(x...)); }
 
  protected:
-  PulseMeterFadeSensor *pulse_meter_;
+  PulseMeterFadeSensor *pulse_meter_fade_;
 };
 
-}  // namespace pulse_meter
+}  // namespace pulse_meter_fade
 }  // namespace esphome
